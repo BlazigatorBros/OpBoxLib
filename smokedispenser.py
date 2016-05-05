d4f6297382f4d526778bf165880a65571dfbfee3
ntopper <nicktopper@gmail.com>
2016-05-05 14:16:32 -0400

    implemented new smoke box functions

--- smokedispenser.py
+++ smokedispenser.py
@@ -4,19 +4,33 @@ import time
 class SmokeDispenser(Module):
 
     callback = "ERROR_SMOKER_EMPTY"
-    dose_command = "doses"
+    burn_command = "burn"
+    load_command = "load"
+    empty_command = "empty"
 
     def __init__(self):
         super(SmokeDispenser, self).__init__(SmokeDispenser.callback)
 
-    def dose(self, duration):
+    def burn(self):
         self.parent.serial.write(
-                SmokeDispenser.dose_command 
-                + " " 
-                + str(duration) 
+                SmokeDispenser.burn_command 
                 + '\n'
         )
 
-        time.sleep(duration / 1000.0 * 2)
+        return self.parent.poll()
+
+    def load(self):
+        self.parent.serial.write(
+                SmokeDispenser.load_command 
+                + '\n'
+        )
+
+        return self.parent.poll()
+
+    def empty(self):
+        self.parent.serial.write(
+                SmokeDispenser.empty_command 
+                + '\n'
+        )
 
         return self.parent.poll()
